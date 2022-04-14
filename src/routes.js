9150a435391a016b1817170cb09a42d1da9a4160
Gabriel Christian <gabrielcwdo18@gmail.com>
2022-04-14 17:55:04 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,39 @@
+const {
+  addBookHandler,
+  getAllBooksHandler,
+  getBooksByIdHandler,
+  deleteBooksByIdHandler,
+  editBooksByIdHandler,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBookHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooksHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books/{bookId}',
+    handler: getBooksByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{bookId}',
+    handler: deleteBooksByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{bookId}',
+    handler: editBooksByIdHandler,
+  },
+];
+
+module.exports = {
+  routes,
+};
